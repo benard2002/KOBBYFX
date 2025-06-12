c73c0041d444374e5040c75cef1bd85a7b0f5917
Bernard <ambenard2002@gmail.com>
2025-06-12 14:00:08 +0000

    hamburger

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const navMenu = document.querySelector(".nav-menu")
+
 const hamburger = document.querySelector(".hamburger")
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
